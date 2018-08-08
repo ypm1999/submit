b9771840810cdfa8e42be170bd2415f14f451a1e
ypm1999 <yang1999422@163.com>
2018-08-08 15:19:19 +0800

    before_final

--- tensorlow/CNN_c/OpenBLAS_install.sh
+++ tensorlow/CNN_c/OpenBLAS_install.sh
@@ -4,5 +4,7 @@ cd OpenBLAS
 make FC=gfortran
 make PREFIX=/usr/local/Openblas install
 cp -f /usr/local/Openblas/include/* /usr/local/include/
-rm -rf /usr/local/lib/libopenblas.a
-ln -s /usr/local/Openblas/lib/libopenblas_haswellp-r0.3.3.dev.a /usr/local/lib/libopenblas.a
+rm -f /usr/local/lib/libopenblas.a
+rm -f /usr/local/lib/libopenblas.so
+ln -f -s /usr/local/Openblas/lib/libopenblas_haswellp-r0.3.3.dev.a /usr/local/lib/libopenblas.a
+ln -f -s /usr/local/Openblas/lib/libopenblas.so.0 /usr/local/lib/libopenblas.so
